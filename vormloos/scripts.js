3aef38dbe1d65581ac77063283f7596dec23d5e9
norealdirection <57847320+norealdirection@users.noreply.github.com>
2023-04-06 11:08:14 +0200

    Update scripts.js

--- vormloos/scripts.js
+++ vormloos/scripts.js
@@ -1,7 +1,15 @@
 
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.OrthographicCamera(
+          window.innerWidth / -2,
+          window.innerWidth / 2,
+          window.innerHeight / 2,
+          window.innerHeight / -2,
+          1,
+          1000
+        );
+
 camera.position.z = 1000;
 
 var renderer = new THREE.WebGLRenderer();
